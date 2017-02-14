8542411c02c67caf6f38e2f2b69632e46628663c
php 04 <php.04@metralha.cec.ime.usp.br>
2017-02-14 19:46:47 -0200

    commit

---
+++ resources/views/saudacao/ola.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Olá</title>
+
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
+  </head>
+  <body>
+    <div class="container">
+      <h1>Olá</h1>
+    </div>
+
+     <script type="text/javascript" src="/js/app.js" />
+  </body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Olá</title>
+  </head>
+  <body>
+    <h1>OLA</h1>
+
+    <?php for($i=0; $i<100 ; $i++)
+    {  ?>
+      <h1>Olá<h1>
+    <?php }   ?>
+
+  </body>
+</html>
